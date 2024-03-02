e61ba57e4c3cb0df5dce9fd91dee4832763a6de9
FlaviaGoes <flavia04goes@outlook.com>
2024-03-01 22:30:11 -0300

    Atualizações exercícios

--- Exercicios_001/dist/js/index.js
+++ Exercicios_001/dist/js/index.js
@@ -40,45 +40,90 @@ Exercício 3
 */
 function conjuntosMedia(...valores) {
     let soma = 0;
-    for (let i = 0; i <= valores.length; i++) {
+    for (let i = 0; i < valores.length; i++) {
         soma += valores[i];
     }
-    return (soma / valores.length);
+    return soma / valores.length;
 }
+console.log(conjuntosMedia(1, 2, 3, 4, 5));
 /*
-
     Exercício 4
 
     Crie uma função que receba uma string como parâmetro e retorne a mesma
     string com todas as letras em caixa alta. Utilize essa função para converter
     diferentes strings.
-
+*/
+function HighBox(palavra) {
+    return palavra = palavra.toUpperCase();
+}
+console.log(HighBox('Casa'));
+/*
     Exercício 5
 
     Desenvolva uma função que determine se um número é primo ou não. Retorne
     true se for primo e false se não for.
 
+*/
+function NumPrimo(val) {
+    let cont = 0;
+    for (let i = 0; i <= val; i++) {
+        if (val % i == 0)
+            cont++;
+    }
+    if (cont = 2)
+        return true;
+    else
+        return false;
+}
+console.log(NumPrimo(7));
+/*
     Exercício 6
 
     Implemente uma função que inverta a ordem dos elementos em um array.
     Utilize essa função para inverter a ordem de diferentes conjuntos de elementos.
-
+*/
+function Inverso(...val) {
+    return val.reverse();
+}
+console.log(Inverso(1, 2, 3));
+/*
     Exercício 7
 
     Crie uma função que receba um valor e uma porcentagem como parâmetros. A
     função deve retornar o valor acrescido da porcentagem indicada.
-
+*/
+function porcentagem(numero, porcent) {
+    let val = Number(porcent.replace('%', ''));
+    return ((numero * val) / 100);
+}
+console.log(porcentagem(100, '10%'));
+/*
     Exercício 8
     Crie uma função que receba uma string e retorne a mesma string, mas com as
     palavras em ordem reversa.
-
+*/
+function palindromo(palavra) {
+    let word = palavra;
+    const fraseInverso = palavra.split('').reverse().join('');
+    return fraseInverso;
+}
+console.log(palindromo('reconhecer'));
+/*
     Exercício 9
     
     Implemente uma função que retorne a soma de todos os números pares em um
     array.
-
+*/
+/*
     Exercício 10
 
     Crie uma função que calcule o fatorial de um número. Utilize essa função para
     calcular o fatorial de diferentes números.
 */
+function fatorial(num) {
+    for (let i = 1; i <= num; i++) {
+        num *= i;
+    }
+    return num;
+}
+console.log(fatorial(3));
