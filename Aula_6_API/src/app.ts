7c547e113604041138bb0a0a2b7299367375f014
FlaviaGoes <flavia04goes@outlook.com>
2024-03-22 22:29:38 -0300

    Others changes

--- Aula_6_API/src/app.ts
+++ Aula_6_API/src/app.ts
@@ -1,15 +1,17 @@
 import express, {Request, Response} from 'express';
+import {Pessoa} from './model/Pessoa';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
+app.use(express.json());
 
 function appLog(){
     console.log("A API está disponível no URL https://localhost:3000");
 }
 
-function hello(resq: Request, res:Response){
+function hello(req: Request, res:Response){
     return res.status(201).json({mensagem: "Hello World!"});
 }
 
 app.get("/api/hello", hello);
-app.listen(PORT, appLog)
+app.listen(PORT, appLog);
