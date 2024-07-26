a94c651c71f81268dfe78fcc0c0110293527bbbb
FlaviaGoes <flavia04goes@outlook.com>
2024-07-26 08:14:50 -0300

    Criando repositório do tipo conta

--- Aula_19_Relacionamento_Entidades/src/repository/tipo_contaRepository.ts
+++ Aula_19_Relacionamento_Entidades/src/repository/tipo_contaRepository.ts
@@ -1,5 +1,5 @@
 import { executarComandoSQL } from "../database/mysql"
-import { Conta } from "../model/Conta"
+import { TipoConta } from "../model/TipoConta";
 
 export class tipo_conta {
 
@@ -9,10 +9,10 @@ export class tipo_conta {
 
     private async createTableTipo() {
         const query = `
-        CREATE TABLE tipos_conta (
+        CREATE TABLE tipos_Conta (
             id INT PRIMARY KEY,
             descricao VARCHAR (255),
-            codigo_tipo_conta VARCHAR (50) UNIQUE
+            codigo_tipo_conta INT(10) UNIQUE
         )`;
 
         try {
@@ -21,5 +21,21 @@ export class tipo_conta {
         } catch (err) {
             console.error('Error');
         }
-    } 
+    }
+
+    async insertContaTipo(descricao:string, codigo_tipo_conta:number):Promise<TipoConta>{
+        const query = `INSERT INTO banco.tipos_Conta(descricao, codigo_tipo_conta) VALUES (?, ?)`
+
+        try{
+            const resultado = await executarComandoSQL(query, [descricao, codigo_tipo_conta]);
+            console.log('Tipo Conta foi inserido com sucesso, ID:', resultado.insertId);
+            const conta_tipo = new TipoConta(resultado.insertId, descricao, codigo_tipo_conta)
+            return new Promise<TipoConta>((resolve)=>{
+                resolve(conta_tipo);
+            })
+        } catch (err){
+            console.error('Erro ao inserir Tipo COnta', err);
+            throw err;
+        }
+    }
 }
