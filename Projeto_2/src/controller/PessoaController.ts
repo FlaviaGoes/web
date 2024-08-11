40386656a6745c89544b15c223d8923f68f6430d
FlaviaGoes <flavia04goes@outlook.com>
2024-08-11 17:24:29 -0300

    Update Controller

--- Projeto_2/src/controller/PessoaController.ts
+++ Projeto_2/src/controller/PessoaController.ts
@@ -21,6 +21,61 @@ export class pessoaController extends Controller {
             } catch (error: any) {
                 return fail(400, new BasicResponseDto(error.message, undefined));
             }
+    };
+
+    @Put()
+    async atualizarPessoa(
+        @Body() dto: PessoaRequestDto,
+        @Res() fail: TsoaResponse<400, BasicResponseDto>,
+        @Res() success: TsoaResponse<201, BasicResponseDto>
+    ) : Promise < | void> {
+        try {
+            const pessoa = await this.PessoaService.atualizaPessoa(dto);
+            return success(201, new BasicResponseDto("Pessoa atualizada com sucesso!", pessoa));
+        } catch (error:any){
+            return fail(400, new BasicResponseDto(error.message, undefined));
         }
+    };
+
+    @Delete()
+    async deletarPessoa (
+        @Body() dto: PessoaRequestDto,
+        @Res() fail: TsoaResponse<400, BasicResponseDto>,
+        @Res() success: TsoaResponse<201, BasicResponseDto>
+    ) : Promise < | void> {
+        try {
+            const pessoa = await this.PessoaService.deletaPessoa(dto);
+            return success(201, new BasicResponseDto("Pessoa deletada com sucesso!", pessoa));
+        } catch (error: any) {
+            return fail(400, new BasicResponseDto(error.message, undefined));
+        }
+    };
+
+    @Get()
+    async filtrarPessoa (
+        @Query() id: number,
+        @Res() fail: TsoaResponse<400, BasicResponseDto>,
+        @Res() success : TsoaResponse<201, BasicResponseDto>
+    ) : Promise < | void> {
+        try {
+            const pessoa = await this.PessoaService.filtraPessoa(id);
+            return success(201, new BasicResponseDto("Pessoa encontrada com sucesso!", pessoa));
+        } catch (error:any) {
+            return fail(400, new BasicResponseDto(error.message, undefined));
+        }
+    };
+
+    @Get("all")
+    async listarPessoas(
+        @Res() fail: TsoaResponse<400, BasicResponseDto>,
+        @Res() success: TsoaResponse<201, BasicResponseDto>
+    ) : Promise < | void> {
+        try {
+            const pessoas = await this.PessoaService.filtraPessoas();
+            return success(201, new BasicResponseDto("Pessoas listadas com sucesso!", pessoas));
+        } catch (error:any) {
+            return fail(400, new BasicResponseDto(error.message, undefined));
+        }
+    };
 }
 
